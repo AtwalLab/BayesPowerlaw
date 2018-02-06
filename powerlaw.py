836e504c51e1fc7cd68028d2aa9f4de60c5023df
Kristina Grigaityte <kgrigait@Kristina.local>
2018-02-06 14:07:02 -0500

    xmax + 50

--- powerlaw.py
+++ powerlaw.py
@@ -40,58 +40,62 @@ def power_law(exponent, xmax, sample_size, discrete=True):
 
 class Fit(object):
 
-	def __init__(self, data, initial_guess=np.linspace(1,5,10), xmin=1, xmax=np.infty, discrete=True):
-		self.data=np.array(data)
-		self.n=len(data)
-		self.constant=np.sum(np.log(data))/self.n
-		self.xmin=xmin
-		self.xmax=xmax
-		self.discrete=discrete
-		if self.discrete==True:
-			self.initial_guess=initial_guess
-			self.best_guess=self.Guess_discrete()
-		else:
-			self.best_guess=self.Guess_continuous()
+    def __init__(self, data, initial_guess=np.linspace(1,5,10), xmin=1, xmax=None, discrete=True):
+        self.data=data
+        self.xmin=xmin
+        if xmax is None:
+            self.xmax=max(self.data)+50
+        else:
+            self.xmax=xmax
+        if self.xmin>1 or self.xmax!=np.infty:
+            self.data=self.data[(self.data>=self.xmin) & (self.data<=self.xmax)]
+        self.n=len(self.data)
+        self.constant=np.sum(np.log(self.data))/self.n
+        self.discrete=discrete
+        if self.discrete==True:
+            self.initial_guess=initial_guess
+            self.best_guess=self.Guess_discrete()
+        else:
+            self.best_guess=self.Guess_continuous()
 
-	def Z(self,gamma,xmin=1,xmax=np.infty):
-	    """The normalization function Z.
-	    Note that default arguments of xmin and xmax make Z equivalent to Riemann zeta function which is already
-	    implemented in Scipy as zeta(gamma,1)"""
-	    if np.isfinite(xmax):
-	        s=0
-	        for i in xrange(xmin,xmax+1):
-	            s+=(1/(i**gamma))
-	    else:
-	        s=zeta(gamma,xmin)
-	    return s
+    def Z(self,gamma,xmin=1,xmax=np.infty):
+        """The normalization function Z.
+        Note that default arguments of xmin and xmax make Z equivalent to Riemann zeta function which is already
+        implemented in Scipy as zeta(gamma,1)"""
+        if np.isfinite(xmax):
+            s=0
+            for i in xrange(xmin,xmax+1):
+                s+=(1/(i**gamma))
+        else:
+            s=zeta(gamma,xmin)
+        return s
 
+    def F(self,gamma):
+        """The optimization function F(gamma). C is the second term in the definition of F(gamma) and is independent of
+        gamma. C need to be defined before running the function. Function Z must be run beforehand as well."""
+        h = 1e-8
+        Z_prime = (self.Z(gamma+h,self.xmin,self.xmax) - self.Z(gamma-h,self.xmin,self.xmax))/(2*h)
+        return (Z_prime/self.Z(gamma,self.xmin,self.xmax))+self.constant
 
-	def F(self,gamma):
-	    """The optimization function F(gamma). C is the second term in the definition of F(gamma) and is independent of
-	    gamma. C need to be defined before running the function. Function Z must be run beforehand as well."""
-	    h = 1e-8
-	    Z_prime = (self.Z(gamma+h,self.xmin,self.xmax) - self.Z(gamma-h,self.xmin,self.xmax))/(2*h)
-	    return (Z_prime/self.Z(gamma,self.xmin,self.xmax))+self.constant
+    def Guess_discrete(self):
+        best_guess=np.zeros(len(self.initial_guess))
+        for i in range(len(self.initial_guess)):
+            try:
+                best_guess[i]=newton(self.F,self.initial_guess[i])
+            except RuntimeError: best_guess[i]=0
+        best_guess=best_guess[best_guess!=0]
+        log_likelihood=np.zeros(len(best_guess))
+        for i in range(len(best_guess)):
+            log_likelihood[i]=-self.n*np.log(self.Z(best_guess[i]))-best_guess[i]*np.sum(np.log(self.data))
+        self.log_likelihood=log_likelihood
+        if len(log_likelihood)==0:
+            best_guess=0
+        else:
+            best_guess=best_guess[np.where(log_likelihood == max(log_likelihood))]
+        return best_guess
 
-	def Guess_discrete(self):
-		best_guess=np.zeros(len(self.initial_guess))
-		for i in range(len(self.initial_guess)):
-			try:
-				best_guess[i]=newton(self.F,self.initial_guess[i])
-			except RuntimeError: best_guess[i]=0
-		best_guess=best_guess[best_guess!=0]
-		log_likelihood=np.zeros(len(best_guess))
-		for i in range(len(best_guess)):
-			log_likelihood[i]=-self.n*np.log(self.Z(best_guess[i]))-best_guess[i]*np.sum(np.log(self.data))
-		self.log_likelihood=log_likelihood
-		if len(log_likelihood)==0:
-			best_guess=0
-		else:
-		    best_guess=best_guess[np.where(log_likelihood == max(log_likelihood))]
-		return best_guess
-
-	def Guess_continuous(self):
-		return 1+self.n/(np.sum(np.log(self.data))-self.n*np.log(self.xmin))
+    def Guess_continuous(self):
+        return 1+self.n/(np.sum(np.log(self.data))-self.n*np.log(self.xmin))
 
 
 class Fit_Bayes(object):
@@ -99,7 +103,7 @@ class Fit_Bayes(object):
         self.data=data
         self.xmin=xmin
         if xmax is None:
-            self.xmax=max(100.0,max(self.data))
+            self.xmax=max(self.data)+50
         else:
             self.xmax=xmax
         if self.xmin>1 or self.xmax!=np.infty:
