100311698a6c195aedb37f3a932650411d6610bd
Kristina Grigaityte <kgrigait@kristina.cshl.edu>
2018-08-14 15:25:46 -0400

    add modules to conf.py

--- BayesPowerlaw/docs/conf.py
+++ BayesPowerlaw/docs/conf.py
@@ -18,7 +18,7 @@ sys.path.insert(0, os.path.abspath('.'))
 
 import mock
  
-MOCK_MODULES = ['numpy', 'scipy', 'matplotlib', 'matplotlib.pyplot']
+MOCK_MODULES = ['numpy', 'scipy', 'matplotlib', 'matplotlib.pyplot','scipy.special','scipy.optimize','scipy.stats']
 for mod_name in MOCK_MODULES:
     sys.modules[mod_name] = mock.Mock()
 
