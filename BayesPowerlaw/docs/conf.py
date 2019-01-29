f92941813b37011799e271a04475adf34754bf9f
Kristina Grigaityte <kgrigait@kristinag.fios-router.home>
2019-01-28 20:15:11 -0500

    fixed version

--- BayesPowerlaw/docs/conf.py
+++ BayesPowerlaw/docs/conf.py
@@ -33,9 +33,9 @@ copyright = '2018, Kristina Grigaityte, Atwal S. Gurinder'
 author = 'Kristina Grigaityte, Atwal S. Gurinder'
 
 # The short X.Y version
-version = '0.6'
+version = '0.8'
 # The full version, including alpha/beta/rc tags
-release = '0.6b'
+release = '0.8b'
 
 
 # -- General configuration ---------------------------------------------------
